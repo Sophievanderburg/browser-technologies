5985e1d27a10a4cb1e08d7a21725df375edff083
Sophie van der Burg <sophievdburg9@gmail.com>
2022-03-31 15:56:40 +0200

    post method

--- opdracht-1/eindopdracht/server.js
+++ opdracht-1/eindopdracht/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 
 const app = express()
 const port = 8000
@@ -11,14 +13,28 @@ app.set('views', './views');
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
+
+/*--------------------- Routes ------------------------- */
 app.get("/", renderPagina)
+app.post("/", verzenden)
 
-
+app.get("/verzonden", renderVerzenden)
 
 /*--------------------- Functions ------------------------- */
 function renderPagina (req, res){
     res.render('index')
+}
+
+function verzenden (req, res){
+    console.log(req.body)
+    res.redirect('/verzonden');
+}
+
+function renderVerzenden(req, res){
+    res.render('verzonden')
 }
 
 app.listen(port, () => {
